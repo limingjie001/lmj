fcd7a0a380cf91aa88606ee8888faffc13aff2c0
lmj <2906776925@qq.com>
2020-10-23 08:50:34 +0800

    测试push

---
+++ src/B.java
@@ -0,0 +1,11 @@
+
+    public static void main(String[] args) {
+        B b = new B();
+        A a = b.getA();
+        int c = 1;
+        System.out.println(a.getAge(c));//使用B类方法局部变量
+    }
+//测试push功能
+
+
+}
