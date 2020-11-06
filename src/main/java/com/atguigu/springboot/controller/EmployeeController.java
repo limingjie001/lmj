455e1e70069fe9ffae82104ec895fe3d9e1251c1
lmj <2906776925@qq.com>
2020-11-06 21:44:12 +0800

    springboot尚硅谷（第三次加入笔记）

--- src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -41,7 +41,8 @@ public class EmployeeController {
     }
 
     //员工添加
-    //！！SpringMVC自动将请求参数和入参对象的属性进行一一绑定；要求请求参数的名字和javaBean入参的对象里面的属性名是一样的
+    //！！SpringMVC自动将请求参数和入参对象的属性进行一一绑定；
+    // 要求请求参数的名字和javaBean入参的对象里面的”属性名“是一样的
     @PostMapping("/emp")
     public String addEmp(Employee employee){
         //来到员工列表页面
@@ -55,7 +56,8 @@ public class EmployeeController {
     }
 
     //来到修改页面，查出当前员工，在页面回显
-    @GetMapping("/emp/{id}")
+    @GetMapping("/emp/{id}")//路径变量
+//    获取路径变量
     public String toEditPage(@PathVariable("id") Integer id,Model model){
         Employee employee = employeeDao.get(id);
         model.addAttribute("emp",employee);
