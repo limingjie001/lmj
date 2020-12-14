ec44052ce5a0108e6bbbaa6d9c5995d2f48c69d2
lmj <2906776925@qq.com>
2020-12-14 15:32:32 +0800

    计算方法第一次实验2

--- src/com/company/Test1_1.java
+++ src/com/company/Test1_1.java
@@ -7,11 +7,11 @@ public class Test1_1 {
     public static void main(String[] args) {
         float y_0 = (float) (log(6.0) - log(5.0)), y_1;
         int n = 1;
-        System.out.printf("y[0]=-%20f", y_0);
+        System.out.printf("y[0]=%-20f", y_0);
 
         while (true) {
-            y_1 = (float) ((1.0 / n) - (5 * y_0));
-            System.out.printf("y[%d]=-%20f", n, y_1);
+            y_1 = (float)((1.0 / n) - (5 * y_0));
+            System.out.printf("y[%d]=%-20f", n, y_1);
             if (n >= 20) break;
             y_0 = y_1;
             n++;
