0cc002b762da56d40d456d22fe8484bbb33888f6
lmj <2906776925@qq.com>
2021-03-18 18:09:24 +0800

    加入了springAop的部分2

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -6,9 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
     @Test
     public void TestAop1(){
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("aop.xml");
         UserService userService=(UserService) applicationContext.getBean("userService");
         userService.search();
+    }
+
+    @Test
+    public void TestAop2(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("aop.xml");
+        UserService userService=(UserService) applicationContext.getBean("userService");
+        userService.add();
     }
 
 }
