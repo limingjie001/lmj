4315ccec2d2234c51e7b7ae51100c0c1a20efe71
lmj <2906776925@qq.com>
2021-03-23 16:04:58 +0800

    Callable配合线程池创建练习

--- src/main/java/com/multi_threading/pool/PoolCallableTest.java
+++ src/main/java/com/multi_threading/pool/PoolCallableTest.java
@@ -1,41 +1,63 @@
 package com.multi_threading.pool;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
+
 /*
     Callable接口配合线程池使用
  */
+//实现callable接口
 public class PoolCallableTest implements Callable {
 
+    private int i;
 
 
-    @Override
-    public Boolean call() throws Exception {
-        System.out.println(Thread.currentThread().getName());
-        return true;
+    public PoolCallableTest(int i) {
+        this.i = i;
+
     }
 
+    //重写call方法
+    @Override
+    public String call() throws Exception {
+        for (int i = 999999; i > 0; i--) ;
+        return "Callable执行,id为" + i;
+    }
 
     public static void main(String[] args) {
-        ExecutorService service= Executors.newFixedThreadPool(5);
 
-        PoolCallableTest t1 = new PoolCallableTest();
-        PoolCallableTest t2 = new PoolCallableTest();
+        //创建执行服务
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
-        Future<Boolean> future=service.submit(t1);
-        Future<Boolean> future2=service.submit(t2);
 
-        try {
-            Boolean aBoolean = future.get();
-            System.out.println(aBoolean);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        List<Future<String>> arrayList = new ArrayList<>();
+        //提交执行
+        for (int i = 0; i < 10; i++) {
+            //new创建目标对象
+            Future<String> future = executorService.submit((new PoolCallableTest(i)));
+            arrayList.add(future);
         }
 
-        service.shutdown();
 
+//获取值需要抛异常
+        for (Future<String> stringFuture : arrayList) {
+            try {
+                System.out.println(stringFuture.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+//关闭服务
+        executorService.shutdown();
     }
 
 
 }
+
+
+
+
+
